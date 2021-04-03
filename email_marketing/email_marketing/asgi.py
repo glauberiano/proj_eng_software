e186f626f33a1aa1bc816d7d8af3e19def085bb1
iegawa <fabianaiegawa@gmail.com>
2021-04-03 15:46:50 -0300

    fix: heroku settings

--- email_marketing/email_marketing/asgi.py
+++ email_marketing/email_marketing/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_marketing.email_marketing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_marketing.email_marketing.settings.base')
 
 application = get_asgi_application()
